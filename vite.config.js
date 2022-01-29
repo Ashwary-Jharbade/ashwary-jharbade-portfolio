e96ed00ec6b828e6503a25a0c132ef84a410ae00
Ashwary <ashwary.jharbade999@gmail.com>
2022-01-29 22:52:50 +0530

    Added few changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "vite-preset-react";
 import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "ashwary-jharbade-portfolio",
+  base: "/ashwary-jharbade-portfolio/",
   plugins: [react(), svgr()],
 });
