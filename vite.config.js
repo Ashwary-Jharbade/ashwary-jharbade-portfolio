efcd95bb4d1f85bf29bc2eac472bf10e0abbc8d6
Ashwary <ashwary.jharbade999@gmail.com>
2022-02-01 13:34:13 +0530

    Removed base property

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from "vite-preset-react";
 import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ashwary-jharbade-portfolio/",
   plugins: [react(), svgr()],
 });
